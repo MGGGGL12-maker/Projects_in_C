416993bd679ef1d7a6b10ec9cbaba2ef22d7b3d6
Joao MIguel <jmocarvallho7123@gmail.com>
2025-08-15 20:38:23 -0300

    Add 'Set_Teams' and 'Game_Scene' functions

--- SRPG_Project/game.c
+++ SRPG_Project/game.c
@@ -56,17 +56,88 @@ void Start_Teams(Player* team, int qtd_players, int team_id)
 }
 
 
+int Set_Teams(int* time_team)
+{
+    int random_num = rand()%2 + 1;
+    *time_team  = random_num;
+    return *time_team; 
+}
+
+
+void Game_Scene(Player* attacking_team, Player* defensing_team, int qtd_players, int attack_team_id)
+{
+    for (int i = 0; i < qtd_players; i++)
+    {
+        printf("\033[1;33m===================================\033[m\n");
+        
+        if (attack_team_id == 1)
+        {
+            printf("TEAM: \033[1;31mRED\033[m\n");
+        }
+        else if (attack_team_id == 2)
+        {
+            printf("TEAM: \033[1;36mBLUE\033[m\n");
+        }
+
+        printf("WAR NAME: %s\n", attacking_team[i].name);
+
+        printf("DEFENSE: %.2f\n", attacking_team[i].defense);
+
+        printf("ATTACK: %.2f\n", attacking_team[i].attack);
+        
+        printf("LIFE: %.2f\n", attacking_team[i].life);
+
+        printf("\033[1;33m===================================\033[m\n\n");
+
+        for (int c = 0; c < qtd_players; c++)
+        {
+            printf("\033[1;33m===================================\033[m\n");
+
+            if (attack_team_id == 1)
+            {
+                printf("TEAM: \033[1;36mBLUE\033[m\n");
+            }
+            else if (attack_team_id == 2)
+            {
+                printf("TEAM: \033[1;31mRED\033[m\n");
+            }
+
+            printf("WAR NAME: %s\n", defensing_team[c].name);
+
+            printf("LIFE: %.2f\n", attacking_team[c].life);
+         
+            printf("\033[1;33m===================================\033[m\n");
+        }
+    }
+
+}
+
+
 int main()
 {
     srand(time(NULL));
 
     // Variables
     int qtd_players = 4;
+    int time_team = 0; // time_team = (1 - Red Team / 2 - Blue Team)
 
     // Vectors
     Player red_team[qtd_players];
     Player blue_team[qtd_players];
 
-    Start_Teams(red_team, qtd_players, 2);
+    Start_Teams(red_team, qtd_players, 1);
+    system("clear");
+    Start_Teams(blue_team, qtd_players, 2);
+    system("clear");
 
+    time_team = Set_Teams(&time_team);
+
+    if (time_team == 1)
+    {
+        Game_Scene(red_team, blue_team, qtd_players, 1);
+    }
+    else if (time_team == 2)
+    {
+        Game_Scene(blue_team, red_team, qtd_players, 2);
+    }
 }
